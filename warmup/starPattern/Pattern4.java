276a24ed15a7804ff7ae4a806e31e7882d78a4db
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-12-18 21:23:16 +0530

    Pattern Programming

---
+++ warmup/starPattern/Pattern4.java
@@ -0,0 +1,25 @@
+package warmup.starPattern;
+
+/*
+
+1 2 3 4 5 6 7 8
+1 2 3 4 5 6 7
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+ */
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n =10;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j<n-i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
